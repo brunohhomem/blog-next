14e2bf750a518f2c7094711265a2b820cd2a351e
brunohhomem <brunohhomem@gmail.com>
2025-08-07 05:19:57 -0300

    feat: add postRepository, spin loader and postsList with suspense tag

--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -1,4 +1,4 @@
-import { PostModel } from "./post-model";
+import { PostModel } from "../../models/post/post-model";
 import { resolve } from "path";
 import { readFile } from "fs/promises";
 import { PostRepository } from "@/repositories/post/post-repository";
@@ -12,7 +12,15 @@ const JSON_POSTS_FILE_PATH = resolve(
   "posts.json"
 );
 
+const SIMULATE_WAIT_IN_MS = 500;
+
 export class JsonPostRepository implements PostRepository {
+  private async simulateWait() {
+    if (SIMULATE_WAIT_IN_MS <= 0) return;
+
+    await new Promise((resolve) => setTimeout(resolve, SIMULATE_WAIT_IN_MS));
+  }
+
   private async readFromDisk(): Promise<PostModel[]> {
     const jsonContent = await readFile(JSON_POSTS_FILE_PATH, "utf-8");
     const parsedJson = JSON.parse(jsonContent);
@@ -21,11 +29,15 @@ export class JsonPostRepository implements PostRepository {
   }
 
   async findAll(): Promise<PostModel[]> {
+    await this.simulateWait();
+
     const posts = await this.readFromDisk();
     return posts;
   }
 
   async findById(id: string): Promise<PostModel> {
+    await this.simulateWait();
+
     const posts = await this.readFromDisk();
     const post = posts.find((post) => post.id === id);
 
@@ -34,5 +46,3 @@ export class JsonPostRepository implements PostRepository {
     return post;
   }
 }
-
-export const postRepository: PostRepository = new JsonPostRepository();
