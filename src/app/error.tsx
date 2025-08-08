9211c1b2ae20728d5cdcf5e6ba6882dc401a2bbc
brunohhomem <brunohhomem@gmail.com>
2025-08-08 12:15:35 -0300

    feat: add error components and notfound

---
+++ src/app/error.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { ErrorMessage } from "@/components/ErrorMessage";
+import { useEffect } from "react";
+
+type RootErrorPageProps = {
+  error: Error;
+  reset: () => void
+}
+
+export default function RootErrorPage({ error, reset }: RootErrorPageProps) {
+  useEffect(() => {
+    console.log(error)
+  }, [error])
+
+  return (
+    <ErrorMessage
+      pageTitle="Erro"
+      contentTitle="Algo de errado não está certo, estamos resolvendo 🚧"
+      content={
+        <button onClick={() => reset()}>
+          Tente outra vez
+        </button>
+      } />
+  )
+}
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,20 +1,10 @@
-import clsx from "clsx";
+import { ErrorMessage } from "@/components/ErrorMessage";
 
 export default function NotFound() {
   return (
-    <>
-      <title>Página não encontrada!</title>
-      <div
-        className={clsx(
-          'min-h-[320px] bg-slate-900 text-slate-100',
-          'mb-16 p-8 rounded-xl',
-          'flex items-center justify-center',
-          'text-center')}>
-        <div>
-          <h1 className="text-7xl/tight mb-4 font-extrabold">404</h1>
-          <p>A página que você está tentando acessar não existe.</p>
-        </div>
-      </div>
-    </>
+    <ErrorMessage
+      pageTitle="Página não encontrada!"
+      contentTitle="404 😅"
+      content="A página que você está tentando acessar não existe." />
   )
 }
