389b991f7220818997f1221fbb06af384cad8d84
brunohhomem <brunohhomem@gmail.com>
2025-08-11 15:16:44 -0300

    feat: add delete-post-action

--- src/actions/post/delete-post-action.ts
+++ src/actions/post/delete-post-action.ts
@@ -1,9 +1,43 @@
 "use server";
 
+import { drizzleDb } from "@/db/drizzle";
+import { postsTable } from "@/db/drizzle/schemas";
+import { asyncDelay } from "@/lib/async-delay";
+import { postRepository } from "@/repositories/post";
+
 import { logColor } from "@/utils/log-color";
+import { eq } from "drizzle-orm";
+import { revalidateTag } from "next/cache";
 
-export async function deletePostAction(formData: FormData) {
-  const id = formData.get("id");
+export async function deletePostAction(id: string) {
+  // TODO: checar login do usuário
 
+  // TODO: remover linhas abaixo
+  await asyncDelay(2000);
   logColor("" + id);
+
+  if (!id || typeof id !== "string") {
+    return {
+      error: "Dados inválidos",
+    };
+  }
+
+  const post = await postRepository.findById(id).catch(() => undefined);
+
+  if (!post) {
+    return {
+      error: "Post não existe",
+    };
+  }
+
+  // TODO: mover este método para o repositório
+  await drizzleDb.delete(postsTable).where(eq(postsTable.id, id));
+
+  // TODO: revalidateTag ou revalidatePath
+  revalidateTag("posts");
+  revalidateTag(`post-${post.slug}`);
+
+  return {
+    error: "",
+  };
 }
--- src/db/drizzle/seed.ts
+++ src/db/drizzle/seed.ts
@@ -7,9 +7,17 @@ import { postsTable } from "./schemas";
   const posts = await jsonPostRepository.findAll();
 
   try {
-    await drizzleDb.delete(postsTable);
+    await drizzleDb.delete(postsTable); // ISSO LIMPA A BASE DE DADOS
     await drizzleDb.insert(postsTable).values(posts);
+
+    console.log();
+    console.log(`${posts.length} posts foram salvos na base de dados.`);
+    console.log();
   } catch (e) {
+    console.log();
+    console.log("Ocorreu um erro...");
+    console.log();
     console.log(e);
+    console.log();
   }
 })();
