031f40cdc17d80dd626cc20b35a207bf1ffc804e
brunohhomem <brunohhomem@gmail.com>
2025-08-13 12:28:16 -0300

    feat: add `CreatePostAction`

--- src/actions/post/create-post-action.ts
+++ src/actions/post/create-post-action.ts
@@ -1,15 +1,29 @@
 "use server";
 
+import { PublicPost } from "@/dto/post/dto";
+
 type CreatePostActionState = {
-  numero: number;
+  formState: PublicPost;
+  errors: string[];
 };
 
 export async function createPostAction(
-  prevState: CreatePostActionState
+  prevState: CreatePostActionState,
+  formData: FormData
 ): Promise<CreatePostActionState> {
-  console.log({ prevState });
+  // TODO: verificar se o usuário tá logado
+
+  if (!(formData instanceof FormData)) {
+    return {
+      formState: prevState.formState,
+      errors: ["Dados inválidos"],
+    };
+  }
+
+  const formDataToObj = Object.fromEntries(formData.entries());
 
   return {
-    numero: prevState.numero + 1,
+    formState: prevState.formState,
+    errors: [],
   };
 }
