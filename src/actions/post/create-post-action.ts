caf3e699b1521dcc02a50647a906f7a35549e10c
brunohhomem <brunohhomem@gmail.com>
2025-08-13 14:40:24 -0300

    feat: add utils and create post at db

--- src/actions/post/create-post-action.ts
+++ src/actions/post/create-post-action.ts
@@ -1,6 +1,15 @@
 "use server";
 
-import { PublicPost } from "@/dto/post/dto";
+import { drizzleDb } from "@/db/drizzle";
+import { postsTable } from "@/db/drizzle/schemas";
+import { makePartialPublicPost, PublicPost } from "@/dto/post/dto";
+import { PostCreateSchema } from "@/lib/post/validations";
+import { PostModel } from "@/models/post/post-model";
+import { getZodErrorMessages } from "@/utils/get-zod-error-messages";
+import { makeSlugFromText } from "@/utils/make-slug-from-text";
+import { revalidateTag } from "next/cache";
+import { redirect } from "next/navigation";
+import { v4 as uuidV4 } from "uuid";
 
 type CreatePostActionState = {
   formState: PublicPost;
@@ -21,9 +30,28 @@ export async function createPostAction(
   }
 
   const formDataToObj = Object.fromEntries(formData.entries());
+  const zodParsedObj = PostCreateSchema.safeParse(formDataToObj);
 
-  return {
-    formState: prevState.formState,
-    errors: [],
+  if (!zodParsedObj.success) {
+    const errors = getZodErrorMessages(zodParsedObj.error.format());
+    return {
+      errors,
+      formState: makePartialPublicPost(formDataToObj),
+    };
+  }
+
+  const validPostData = zodParsedObj.data;
+  const newPost: PostModel = {
+    ...validPostData,
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+    id: uuidV4(),
+    slug: makeSlugFromText(validPostData.title),
   };
+
+  // TODO: mover este método para o repositório
+  await drizzleDb.insert(postsTable).values(newPost);
+
+  revalidateTag("posts");
+  redirect(`/admin/post/${newPost.id}`);
 }
--- src/lib/post/validations.ts
+++ src/lib/post/validations.ts
@@ -3,26 +3,26 @@ import sanitizeHtml from "sanitize-html";
 import { z } from "zod";
 
 const PostBaseSchema = z.object({
-  title: z
-    .string()
-    .trim()
-    .min(3, "Título deve ter, no mínimo, 3 caracteres")
-    .max(120, "Título deve ter um máximo de 120 caracteres"),
-  content: z
-    .string()
-    .trim()
-    .min(3, "Conteúdo é obrigatório")
-    .transform((val) => sanitizeHtml(val)),
   author: z
     .string()
     .trim()
     .min(4, "Autor precisa de um mínimo de 4 caracteres")
     .max(100, "Nome do autor não deve ter mais que 100 caracteres"),
+  title: z
+    .string()
+    .trim()
+    .min(3, "Título deve ter, no mínimo, 3 caracteres")
+    .max(120, "Título deve ter um máximo de 120 caracteres"),
   excerpt: z
     .string()
     .trim()
     .min(3, "Excerto precisa de um mínimo de 3 caracteres")
     .max(200, "Excerto não deve ter mais que 200 caracteres"),
+  content: z
+    .string()
+    .trim()
+    .min(3, "Conteúdo é obrigatório")
+    .transform((val) => sanitizeHtml(val)),
   coverImageUrl: z.string().trim().refine(isUrlOrRelativePath, {
     message: "URL da capa deve ser uma URL ou caminho para imagem",
   }),
