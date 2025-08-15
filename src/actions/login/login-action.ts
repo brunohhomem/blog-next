2c75a8705c4504f7ddbb2b4db56ce44784fe619f
brunohhomem <brunohhomem@gmail.com>
2025-08-15 11:03:16 -0300

    feat: add login validation to protected layout and login permission

--- src/actions/login/login-action.ts
+++ src/actions/login/login-action.ts
@@ -10,6 +10,15 @@ type LoginActionState = {
 };
 
 export async function loginAction(state: LoginActionState, formData: FormData) {
+  const allowLogin = Boolean(Number(process.env.ALLOW_LOGIN));
+
+  if (!allowLogin) {
+    return {
+      username: "",
+      error: "Login not allowed",
+    };
+  }
+
   await asyncDelay(1000); // Vou manter
 
   if (!(formData instanceof FormData)) {
--- src/app/admin/post/layout.tsx
+++ src/app/admin/post/layout.tsx
@@ -1,10 +1,13 @@
 import { MenuAdmin } from "@/components/admin/MenuAdmin/indext";
+import { requireLoginSessionOrRedirect } from "@/lib/login/manage-login";
 
 type AdminPostLayoutProps = {
   children: React.ReactNode;
 };
 
-export default function AdminPostLayout({ children }: Readonly<AdminPostLayoutProps>) {
+export default async function AdminPostLayout({ children }: Readonly<AdminPostLayoutProps>) {
+  await requireLoginSessionOrRedirect();
+
   return (
     <>
       <MenuAdmin />
